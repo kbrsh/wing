601bbf7d7b0d71817d3bb76abd962c8073ad6d69
bruhr <simplecooldude1@gmail.com>
2016-06-04 21:15:00 +0000

    :arrow_up_down:

--- watch.js
+++ watch.js
@@ -1,6 +1,7 @@
 var exec = require("child_process").exec;
 var emoji = require('./extra/emoji.js')
 
+// emoji is commit :)
 setInterval(function() {
     exec('git add . && git commit -m "' + emoji.random() + '"');
 }), 1000; 
