8e0a68204f63fdf26f7e52152d95a8a9595f0293
bruhr <simplecooldude1@gmail.com>
2016-07-01 20:54:01 +0000

    no more watch script

--- watch.js
+++
@@ -1,9 +0,0 @@
-var exec = require("child_process").exec;
-var emoji = require('./extra/emoji.js')
-
-// a little script for committing to github
-// an emoji is the commit message :)
-setInterval(function() {
-    exec('git add . && git commit -m "' + emoji.random() + '"');
-}), 1000; 
- 
