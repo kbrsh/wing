93f6b62d0ae08f4da2dc3a0a15565eb53b6077d0
Kabir Shah <simplecooldude1@gmail.com>
2016-07-10 16:19:13 -0700

    Delete watch.js

--- watch.js
+++
@@ -1,6 +0,0 @@
-var exec = require("child_process").exec;
-var emoji = require('./extra/emoji.js')
-
-setInterval(function() {
-    exec('git add . && git commit -m "' + emoji.random() + '"');
-}), 1000;
