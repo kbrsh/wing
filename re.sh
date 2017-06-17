b175b93a529d3cc832b0c0b9493489db76bdc1dc
Kabir Shah <simplecooldude1@gmail.com>
2017-06-17 10:45:53 +0530

    change

---
+++ re.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+OLD_NAME="Gopal Shah"
+CORRECT_NAME="Kabir Shah"
+CORRECT_EMAIL="simplecooldude1@gmail.com"
+if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
