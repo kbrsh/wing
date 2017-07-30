ad8a3c1f6c64f0fd95746d0960c5707dad233a5f
Kabir Shah <simplecooldude1@gmail.com>
2017-07-29 17:04:03 -0700

    syntax highlighting

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,46 @@
+var STR_RE = /(["'`])((?:.|\n)*?)\1/g;
+var HTML_COMMENT_RE = /(\&lt;\!\-\-(?:(?:.|\n)*)\-\-\&gt;)/g;
+var HTML_ATTRIBUTE_RE = /(\S+)=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/g;
+var HTML_TAG_RE = /(&lt;\/?[\w\d-]*?)(\s(?:.|\n)*?)?(\/?&gt;)/g;
+
+var code = document.getElementsByTagName("code");
+
+var compile = function(val, lang) {
+  var compiled = val;
+
+  compiled = compiled.replace(STR_RE, "<span class=\"string\">$1$2$1</span>");
+
+  compiled = compiled.replace(HTML_COMMENT_RE, "<span class=\"comment\">$1</span>");
+  compiled = compiled.replace(HTML_TAG_RE, function(match, start, content, end) {
+    if(content === undefined) {
+      content = "";
+    } else {
+      content = content.replace(HTML_ATTRIBUTE_RE, function(match, name, value) {
+        if(value === "string") {
+          return match;
+        } else {
+          if(value === undefined) {
+            value = "";
+          } else {
+            value = "=" + value;
+          }
+          return "<span class=\"global\">" + name + "</span>" + value;
+        }
+      });
+    }
+
+    return "<span class=\"method\">" + start + "</span>" + content + "<span class=\"method\">" + end + "</span>";
+  });
+
+
+  return compiled;
+}
+
+for(var i = 0; i < code.length; i++) {
+  var el = code[i];
+  el.innerHTML = compile(el.innerHTML, 'html');
+}
+
 new Moon({
   el: "#grid-demo",
   data: {
